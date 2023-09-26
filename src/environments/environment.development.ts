a453c73f771b795a68044f71d2113e5a6a667bfb
DallBia <tibianchini.livros@gmail.com>
2023-09-26 02:19:22 -0300

    outra tentativa de chamar api

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,12 +1,16 @@
 
 
-export const environment = {
-  production: true,
-  ApiUrl: 'http://clinica-api/api'
-};
-
+// export const environment = {
+//   production: true,
+//   ApiUrl: 'http://clinica-api/api'
+// };
+// http://34.123.211.220/
 
 // export const environment = {
 
 //   ApiUrl: "https://localhost:7298/api"
 // };
+export const environment = {
+  production: true,
+  ApiUrl: 'http://34.123.211.220/api'
+};
