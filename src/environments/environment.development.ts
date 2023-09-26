d1030d846a04805e6f7f490f8244ccce5b171b59
DallBia <tibianchini.livros@gmail.com>
2023-09-26 02:03:50 -0300

    ajustando environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 
 
 export const environment = {
-  production: false,
+  production: true,
   ApiUrl: 'http://clinica-api/api'
 };
 
--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
-    production: false,
+    production: true,
     ApiUrl: 'http://clinica-api/api'
 };
 
