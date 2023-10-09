724e0e09910212ac93014d59f8dea187fe9de374
DallBia <tibianchini.livros@gmail.com>
2023-10-09 12:09:37 -0300

    ajustado

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,8 @@
 
 export const environment = {
 
-  ApiUrl: "https://localhost:7298/api"
+  // ApiUrl: "https://localhost:7298/api"
+  ApiUrl: 'http://34.123.211.220:80/api'
 };
 // export const environment = {
 //   production: true,
