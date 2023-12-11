55d7dfcecf918efb11133b3155ab0fd519157c8a
DallBia <tibianchini.livros@gmail.com>
2023-12-11 15:38:04 -0300

    VALOR002

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,17 @@
 
 
+// export const environment = {
+//   ApiUrl: "https://localhost:7298/api"
+// };
+
+
+
 export const environment = {
-  ApiUrl: "https://localhost:7298/api"
+  production: true,
+  ApiUrl: 'http://34.123.211.220:80/api'
 };
 
 
-
-// export const environment = {
-//   production: true,
-//   ApiUrl: 'http://34.123.211.220:80/api'
-// };
+//=========================================
+//  ALTERAR A VERSAO NA TELA DE LOGIN!
+//=========================================
